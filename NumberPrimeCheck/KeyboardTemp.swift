ce2dcab6075e44f21065e131db1ca91b1072b872
Macbook Pro <silvasterantony@Macbooks-MacBook-Pro.local>
2020-02-29 16:39:54 +0530

    Intendation  fix in keyboardTemp

--- NumberPrimeCheck/KeyboardTemp.swift
+++ NumberPrimeCheck/KeyboardTemp.swift
@@ -10,47 +10,37 @@ import Foundation
 import UIKit
 
 protocol KeyboardTempProtocol: class {
-    
-    var tempView : UIView { get }
-    func register()
-    func unRegister()
-    
+  var tempView : UIView { get }
+  func register()
+  func unRegister()
 }
 
 extension KeyboardTempProtocol  {
-    
-    func register() {
-        
-        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: nil) { (notif) in
-            self.keyboardWillShow(notification: notif)
-        }
-        
-        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: nil) { (notif) in
-            self.keyboardWillHide(notification: notif)
-        }
-        
+  
+  func register() {
+    NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: nil) { (notif) in
+      self.keyboardWillShow(notification: notif)
     }
-    
-    func keyboardWillShow(notification: Notification) {
-        
-        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
-            return
-        }
-        
-        // move the root view up by the distance of keyboard height
-        self.tempView.frame.origin.y = 0 - keyboardSize.height
+    NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: nil) { (notif) in
+      self.keyboardWillHide(notification: notif)
     }
-    
-    func keyboardWillHide(notification: Notification) {
-        // move back the root view origin to zero
-        self.tempView.frame.origin.y = 0
+  }
+  
+  func keyboardWillShow(notification: Notification) {
+    guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+      return
     }
-    
-    func unRegister() {
-        
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-    
-    
+    // move the root view up by the distance of keyboard height
+    self.tempView.frame.origin.y = 0 - keyboardSize.height
+  }
+  
+  func keyboardWillHide(notification: Notification) {
+    // move back the root view origin to zero
+    self.tempView.frame.origin.y = 0
+  }
+  
+  func unRegister() {
+    NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+    NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+  }
 }
