dd10ed4f017ca5f00908f1cb1c77b73fa0d5377e
Ezhil <ezhil.american@gmail.com>
2020-02-29 16:39:56 +0530

    ViewController tap gesture removed

--- NumberPrimeCheck/ViewController.swift
+++ NumberPrimeCheck/ViewController.swift
@@ -9,36 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    
-    @IBOutlet weak var testTxtFld: UITextField!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
-        
-        view.addGestureRecognizer(tap)
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        self.register()
-    }
-    
-    @objc func dismissKeyboard() {
-        view.endEditing(true)
-    }
-    
+  
+  @IBOutlet weak var testTxtFld: UITextField!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+   
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    self.register()
+  }
 }
 
 extension ViewController : KeyboardTempProtocol {
-    
-    var tempView: UIView {
-       return self.view
-    }
-    
-    
+  
+  var tempView: UIView {
+    return self.view
+  }
 }
 
